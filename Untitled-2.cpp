9752b8b15541aa02c307162ba770dd7743430545
shubham22062 <shubhamawasthi021@gmail.com>
2023-04-15 18:22:29 +0530

    15-04-2023

---
+++ Untitled-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c cout << "enter 2 nos";
+    cin >> a >> b
+                    c = a + b;
+    cout << "addtion is " << c;
+    rn 0
+}
---
+++ fun.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cout << "enter 2 nos";
+    cin >> a >> b;
+    c = a + b;
+    cout << "addition is " << c;
+    return 0;
+}
